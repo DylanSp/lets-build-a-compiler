7dd6051db39131371f5b1f04560efa1e3f83d368
dylansp <dylan.richard.sprague@gmail.com>
2016-07-13 13:12:00 +0000

    Created test_generator for automatically creating tests.
    Test_generator created, works with manual Makefile.
    Need to debug Makefile so that make tests picks up on everything in test/generated/.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CC = g++
 CFLAGS = -std=c++11 -Wall -Wextra -Wpedantic -pedantic-errors -g
 
 SRCDIR = src
-TESTSRC = test
+TESTSRC = test/generated
 BUILDDIR = build
 TARGET = 
 SRCEXT = cc
@@ -15,8 +15,8 @@ TESTOBJS = $(patsubst $(TESTDIR)/%,$(BUILDDIR)/%,$(TESTSOURCES:.$(SRCEXT)=.o))
 
 # artifacts from test program
 TEST_ARTS = test/src/compiled_out.cc test/bin/*
-LIB = 
-INC = -I include -I src
+LIB = -pthread 
+INC = -I include -I src -I lib/googletest/googletest/include 
 
 all: $(TARGET)
 
@@ -42,3 +42,31 @@ interactive: $(OBJECTS)
   
 full: $(OBJECTS)
 	$(CC) $(CFLAGS) $^ $(INC) $(LIB) -o bin/full_compiler spikes/full_compiler.cc
+	
+test_generator: $(OBJECTS)
+	$(CC) $(CFLAGS) $^ $(INC) $(LIB) -o bin/test_generator spikes/test_generator.cc
+	
+#tests: $(TESTOBJS) build/SingleConstant.o build/SingleConstantTest.o
+#	$(CC) $(CFLAGS) $(LIB) $(TESTOBJS) lib/googletest/googletest/make/gtest_main.a build/SingleConstantTest.o -o bin/run_tests 
+	
+tests: $(TESTOBJS) build/EmptySource.o build/EmptySourceTest.o
+	$(CC) $(CFLAGS) $(LIB) $(TESTOBJS) lib/googletest/googletest/make/gtest_main.a build/EmptySourceTest.o -o bin/run_tests 
+	
+build/SingleConstant.o: test/generated/SingleConstant.cc
+	$(CC) $(CFLAGS) $(INC) -c -o $@ $<
+	
+build/SingleConstantTest.o: test/generated/SingleConstantTest.cc build/SingleConstant.o
+	$(CC) $(CFLAGS) $(INC) -I test/generated -c -o $@ $<
+	
+build/EmptySource.o: test/generated/EmptySource.cc
+	$(CC) $(CFLAGS) $(INC) -c -o $@ $<
+	
+build/EmptySourceTest.o: test/generated/EmptySourceTest.cc build/EmptySource.o
+	$(CC) $(CFLAGS) $(INC) -I test/generated -c -o $@ $<
+	
+testclass:
+	$(CC) $(CFLAGS) $(INC) -c test/src/TestClass.cc
+	
+testclass_test:
+	$(CC) $(LIB) lib/googletest/googletest/make/gtest_main.a TestClass.o
+	
---
+++ spikes/test_generator.cc
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include "compiler.hh"
+
+//primary test case - GetsCorrectResults
+//checks variable value(s)
+void generate_correct_functionality_test (const std::string class_name, 
+                                          std::map<char, int> expected_values, 
+                                          std::ofstream& ofs) {
+    std::string test_class_name = class_name + "Class";
+    ofs << "TEST_F(" << test_class_name << ", GetsCorrectResults ) {" << '\n';
+    for (auto test : expected_values) {
+        ofs << "EXPECT_EQ(" << test.second << ", tested_object.get_variable('" << test.first << "'));" << '\n';
+    }
+    ofs << "}" << '\n';
+    
+}
+
+
+//secondary test case - EmptiesStack
+//checks that stack is empty after running
+void generate_empties_stack_test (const std::string class_name, std::ofstream& ofs) {
+    std::string test_class_name = class_name + "Class";
+    ofs << "TEST_F(" << test_class_name << ", EmptiesStack ) {" << '\n'; 
+    ofs << "EXPECT_TRUE(tested_object.is_stack_empty());" << '\n';
+    ofs << "}" << '\n';
+}
+
+void generate_test_fixture (const std::string class_name, std::ofstream& ofs) {
+    std::string test_class_name = class_name + "Class";
+    ofs << "#include \"gtest/gtest.h\"" << '\n';
+    ofs << "#include \"" << class_name << ".cc\"" << '\n';
+    ofs << "class " << test_class_name << " : public ::testing::Test {" << '\n';
+    ofs << "public:" << '\n';
+    
+    //define ctor, with run() call 
+    ofs << class_name << " tested_object;" << '\n';
+    ofs << test_class_name << " () : tested_object() {" << '\n';
+    ofs << "tested_object.run();" << '\n';
+    ofs << "}" << '\n';
+    
+    //close test fixture definition
+    ofs << "};" << '\n';
+}
+
+void create_testable_class (const std::string class_name,
+                            const std::vector<std::string> program,
+                            std::ofstream& ofs) {
+    ds_compiler::Compiler my_compiler(ofs);
+    my_compiler.compile_full(program, class_name);
+}
+
+struct test_input_params {
+    std::string class_name;
+    std::vector<std::string> program_source;
+    std::map<char, int> expected_values;
+};
+
+std::vector<test_input_params> initialize_test_params () {
+    std::vector<test_input_params> test_params;
+    
+    std::string empty_source_name("EmptySource");
+    std::vector<std::string> empty_source_program;
+    std::map<char, int> empty_source_expected_values;
+    test_input_params empty_program { empty_source_name, empty_source_program, empty_source_expected_values };
+    test_params.push_back(empty_program);
+    
+    /*
+    std::string single_constant_name("SingleConstant");
+    std::vector<std::string> single_constant_program;
+    single_constant_program.push_back("x = 1");
+    std::map<char, int> single_constant_expected_values;
+    single_constant_expected_values['X'] = 1;
+    test_input_params single_constant { single_constant_name, single_constant_program, single_constant_expected_values };
+    test_params.push_back(single_constant);
+    */
+    
+    return test_params;
+}
+
+int main () {
+    const std::string PROJ_ROOT("/home/ubuntu/workspace/");
+    const std::string CLASS_DIR("test/generated/");
+    const std::string TEST_DIR("test/generated/");
+    
+    std::vector<test_input_params> test_params = initialize_test_params();
+    
+    for (auto i : test_params) {
+        std::string class_file_name(PROJ_ROOT + CLASS_DIR + i.class_name + ".cc");
+        std::ofstream class_ofs(class_file_name, std::ofstream::out);
+        
+        create_testable_class(i.class_name, i.program_source, class_ofs);
+
+        std::string test_file_name(PROJ_ROOT + TEST_DIR + i.class_name + "Test.cc");
+        std::ofstream test_ofs(test_file_name, std::ofstream::out);
+        
+        generate_test_fixture(i.class_name, test_ofs);
+        generate_correct_functionality_test(i.class_name, i.expected_values, test_ofs);
+        generate_empties_stack_test(i.class_name, test_ofs);
+    }
+    
+    return 0;
+}
