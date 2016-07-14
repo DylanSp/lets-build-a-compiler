1c699d7ee8ff9ed4c4b4b542e22f830a31bd547d
dylansp <dylan.richard.sprague@gmail.com>
2016-07-14 01:14:39 +0000

    Updated Compiler to handle multi-character literals.
    Compiler methods now pass around std::strings. Test generation now uses single quotes,
    cpu_variables changed to map<string, int>.
    Added tests for multi-char identifiers, multi-digit numbers.
    Updated TODO notes, formatted and updated documentation on differences.

--- spikes/test_generator.cc
+++ spikes/test_generator.cc
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <string>
 #include "compiler.hh"
 
 //primary test case - GetsCorrectResults
 //checks variable value(s)
 void generate_correct_functionality_test (const std::string class_name, 
-                                          std::map<char, int> expected_values, 
+                                          std::map<std::string, int> expected_values, 
                                           std::ofstream& ofs) {
     std::string test_class_name = class_name + "Class";
     ofs << "TEST_F(" << test_class_name << ", GetsCorrectResults ) {" << '\n';
     for (auto test : expected_values) {
-        ofs << "EXPECT_EQ(" << test.second << ", tested_object.get_variable('" << test.first << "'));" << '\n';
+        ofs << "EXPECT_EQ(" << test.second << ", tested_object.get_variable(\"" << test.first << "\"));" << '\n';
     }
     ofs << "}" << '\n';
     
@@ -54,37 +55,42 @@ void create_testable_class (const std::string class_name,
 struct test_input_params {
     std::string class_name;
     std::vector<std::string> program_source;
-    std::map<char, int> expected_values;
+    std::map<std::string, int> expected_values;
 };
 
 std::vector<test_input_params> initialize_test_params () {
     std::vector<test_input_params> test_params {
         {"EmptyProgram", {}, {}},
-        {"SingleConstant", {"x=1"}, {{'X', 1}}},
-        {"SimpleAddition", {"x=1+1"}, {{'X', 2}}},
-        {"SimpleSubtraction", {"x=4-1"}, {{'X',3}}},
-        {"SimpleMultiplication", {"x=2*2"}, {{'X',4}}},
-        {"SimpleDivision", {"x=4/2"}, {{'X',2}}},
-        {"MultipleAddOps", {"x=1+1-3"}, {{'X',-1}}},
-        {"MultipleMultOps", {"x=4*5/2"}, {{'X',10}}},
-        {"SimpleUnaryMinus", {"x=-2"}, {{'X',-2}}},
-        {"SimpleParens", {"x=(1+2)"}, {{'X',3}}},
-        {"ParensPrecedence", {"x=2*(3+1)"}, {{'X',8}}},
-        {"AddUnaryMinus", {"x=-1+2"}, {{'X',1}}},
-        {"SubtractUnaryMinus", {"x=-1-1"}, {{'X',-2}}},
-        {"MultiplyUnaryMinus", {"x=-1*3"}, {{'X',-3}}},
-        {"DivideUnaryMinus", {"x=-4/1"}, {{'X',-4}}},
-        {"SimpleVariableUse", {"x=1", "y=x"}, {{'X',1}, {'Y', 1}}},
-        {"VariableAddition", {"x=2", "y=x+1"}, {{'X',2}, {'Y', 3}}},
-        {"VariableSubtraction", {"x=3", "y=x-2"}, {{'X',3}, {'Y', 1}}},
-        {"VariableMultiplication", {"x=4", "y=5*x"}, {{'X',4}, {'Y', 20}}},
-        {"VariableDivision", {"x=6", "y=x/2"}, {{'X',6}, {'Y', 3}}},
-        {"DivisionByVariable", {"x=4", "y=8/x"}, {{'X',4}, {'Y', 2}}},
-        {"AddMultPrecedence", {"x=1+2*3"}, {{'X',7}}},
-        {"SubMultPrecedence", {"x=4-5*6"}, {{'X',-26}}},
-        {"AddDivPrecedence", {"x=7+8/2"}, {{'X',11}}},
-        {"SubDivPrecedence", {"x=4-6/3"}, {{'X',2}}},
+        {"SingleConstant", {"x=1"}, {{"X", 1}}},
+        {"SimpleAddition", {"x=1+1"}, {{"X", 2}}},
+        {"SimpleSubtraction", {"x=4-1"}, {{"X",3}}},
+        {"SimpleMultiplication", {"x=2*2"}, {{"X",4}}},
+        {"SimpleDivision", {"x=4/2"}, {{"X",2}}},
+        {"MultipleAddOps", {"x=1+1-3"}, {{"X",-1}}},
+        {"MultipleMultOps", {"x=4*5/2"}, {{"X",10}}},
+        {"SimpleUnaryMinus", {"x=-2"}, {{"X",-2}}},
+        {"SimpleParens", {"x=(1+2)"}, {{"X",3}}},
+        {"ParensPrecedence", {"x=2*(3+1)"}, {{"X",8}}},
+        {"AddUnaryMinus", {"x=-1+2"}, {{"X",1}}},
+        {"SubtractUnaryMinus", {"x=-1-1"}, {{"X",-2}}},
+        {"MultiplyUnaryMinus", {"x=-1*3"}, {{"X",-3}}},
+        {"DivideUnaryMinus", {"x=-4/1"}, {{"X",-4}}},
+        {"SimpleVariableUse", {"x=1", "y=x"}, {{"X",1}, {"Y", 1}}},
+        {"VariableAddition", {"x=2", "y=x+1"}, {{"X",2}, {"Y", 3}}},
+        {"VariableSubtraction", {"x=3", "y=x-2"}, {{"X",3}, {"Y", 1}}},
+        {"VariableMultiplication", {"x=4", "y=5*x"}, {{"X",4}, {"Y", 20}}},
+        {"VariableDivision", {"x=6", "y=x/2"}, {{"X",6}, {"Y", 3}}},
+        {"DivisionByVariable", {"x=4", "y=8/x"}, {{"X",4}, {"Y", 2}}},
+        {"AddMultPrecedence", {"x=1+2*3"}, {{"X", 7}}},
+        {"SubMultPrecedence", {"x=4-5*6"}, {{"X", -26}}},
+        {"AddDivPrecedence", {"x=7+8/2"}, {{"X", 11}}},
+        {"SubDivPrecedence", {"x=4-6/3"}, {{"X", 2}}},
         {"SimpleFunctionCall", {"x=f()"}, {}},
+        {"SimpleMultiCharVariable", {"test=1"}, {{"TEST", 1}}},
+        {"SimpleMultiDigitNum", {"x=23"}, {{"X", 23}}},
+        {"MultiCharMultiDigit", {"test=34"}, {{"TEST", 34}}},
+        {"TheKitchenSink", {"intermediate=12+34-56*8/64","result=-(intermediate+123)*456"}, 
+                           {{"INTERMEDIATE", 39}, {"RESULT", -73872}}},
     };
     
     return test_params;
